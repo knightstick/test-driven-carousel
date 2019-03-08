2a58e86b8dd86f3f5209489ceaba0e8616da54f2
Chris Jewell <chrisjohnjewell@gmail.com>
2019-03-09 08:24:10 +1100

    :sparkles: Initial implementation of CarouselSlide

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,7 @@
 module.exports = {
   plugins: ['react'],
   extends: ['eslint:recommended', 'plugin:react/recommended'],
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
+  parser: 'babel-eslint',
   env: {
     node: true,
   },
@@ -15,7 +9,6 @@ module.exports = {
     quotes: ['error', 'single', { avoidEscape: true }],
     'comma-dangle': ['error', 'always-multiline'],
   },
-  parser: 'babel-eslint',
   settings: {
     react: {
       version: '16.4.2',
