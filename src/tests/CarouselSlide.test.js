59e6bcaf6516d242b4b688ae25691fecde6cbb11
Chris Jewell <chrisjohnjewell@gmail.com>
2019-03-09 15:52:24 +1100

    :white_check_mark: Update tests for style-components

--- src/tests/CarouselSlide.test.js
+++ src/tests/CarouselSlide.test.js
@@ -18,15 +18,15 @@ describe('CarouselSlide', () => {
     expect(wrapper.type()).toBe('figure');
   });
 
-  it('renders an <img> and a <figcaption> as children', () => {
-    expect(wrapper.childAt(0).type()).toBe('img');
+  it('renders an props.Img and a <figcaption> as children', () => {
+    expect(wrapper.childAt(0).type()).toBe(CarouselSlide.defaultProps.Img);
     expect(wrapper.childAt(1).type()).toBe('figcaption');
   });
 
   it('passes `imgUrl` through to the <img>', () => {
     const imgUrl = 'https://example.com/image.png';
     wrapper.setProps({ imgUrl });
-    const img = wrapper.find('img');
+    const img = wrapper.find(CarouselSlide.defaultProps.Img);
     expect(img.prop('src')).toBe(imgUrl);
   });
 
@@ -48,5 +48,20 @@ describe('CarouselSlide', () => {
     expect(wrapper.prop('style')).toBe(style);
     expect(wrapper.prop('onClick')).toBe(onClick);
     expect(wrapper.prop('className')).toBe(className);
+  });
+
+  describe('Img', () => {
+    let mounted;
+    const imgUrl = 'https://example.com/default.jpg';
+
+    beforeEach(() => {
+      mounted = mount(
+        <CarouselSlide.defaultProps.Img src={imgUrl} imgHeight={500} />
+      );
+    });
+
+    it('renders an <img> with the given src', () => {
+      expect(mounted.containsMatchingElement(<img src={imgUrl} />)).toBe(true);
+    });
   });
 });
