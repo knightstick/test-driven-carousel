21346db9fba8ef6584d9775f60f4067bc4d559ad
Chris Jewell <chrisjohnjewell@gmail.com>
2019-03-10 10:45:58 +1100

    :sparkles: Allow the index in HasIndex to be controlled

--- src/tests/HasIndex.test.js
+++ src/tests/HasIndex.test.js
@@ -16,8 +16,33 @@ describe('HasIndex()', () => {
     wrapper = shallow(<MockComponentWithIndex />);
   });
 
-  it('has an initial `index` state of 0', () => {
+  it('has an initial `index` state equal to the `defaultIndex` prop', () => {
     expect(wrapper.state('index')).toBe(0);
+    let wrapper2 = shallow(<MockComponentWithIndex defaultIndex={1} />);
+    expect(wrapper2.state('index')).toBe(1);
+  });
+
+  it('always has `index` state equal to the `index` prop', () => {
+    let wrapperWithInitialIndex = shallow(<MockComponentWithIndex index={1} />);
+    expect(wrapperWithInitialIndex.state('index')).toBe(1);
+    wrapper.setProps({ index: 2 });
+    expect(wrapper.state('index')).toBe(2);
+  });
+
+  it('allows `index` state to change if the `index` prop is unset', () => {
+    let wrapperWithInitialIndex = shallow(<MockComponentWithIndex index={1} />);
+    wrapperWithInitialIndex.setProps({ index: undefined });
+    wrapperWithInitialIndex.setState({ index: 3 });
+    expect(wrapperWithInitialIndex.state('index')).toBe(3);
+  });
+
+  it('calls `onIndexChange` on decrement/increment', () => {
+    const onIndexChange = jest.fn();
+    wrapper.setProps({ index: 0, onIndexChange });
+    wrapper.prop('indexDecrement')(3);
+    expect(onIndexChange).toHaveBeenCalledWith({ target: { value: 2 } });
+    wrapper.prop('indexIncrement')(3);
+    expect(onIndexChange).toHaveBeenCalledWith({ target: { value: 1 } });
   });
 
   it('passes the `index` state down as the `index` prop', () => {
@@ -39,8 +64,8 @@ describe('HasIndex()', () => {
 
   it('has the max `index` state on decrement from 0', () => {
     wrapper.setState({ index: 0 });
-    // wrapper.prop('indexDecrement')(3);
-    expect(wrapper.state('index')).toBe(0);
+    wrapper.prop('indexDecrement')(3);
+    expect(wrapper.state('index')).toBe(2);
   });
 
   it('has the min `index` state on incrememnt from the max', () => {
